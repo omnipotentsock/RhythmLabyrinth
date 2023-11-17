49ca4d32c6ce3e75556b28153860a008949bf35c
House <house@Houses-MacBook-Air.local>
2023-11-17 14:58:13 -0500

    rebased outcomes feature to base AdventureGame

--- AdventureGame/BasicAdventureTest.java
+++ AdventureGame/BasicAdventureTest.java
@@ -1,37 +1,48 @@
-//
-//import java.io.File;
-//import java.io.IOException;
-//
-//import AdventureModel.AdventureGame;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class BasicAdventureTest {
-//    @Test
-//    void getCommandsTest() throws IOException {
-//        AdventureGame game = new AdventureGame("TinyGame");
-//        String commands = game.player.getCurrentRoom().getCommands();
-//        assertEquals("WEST,UP,NORTH,IN,SOUTH,DOWN", commands);
-//    }
-//
-//    @Test
-//    void getObjectString() throws IOException {
-//        AdventureGame game = new AdventureGame("TinyGame");
-//        String objects = game.player.getCurrentRoom().getObjectString();
-//        assertEquals("a water bird", objects);
-//    }
-//
-//    @Test
-//    void getSavedDir() {
-//        String separator = System.getProperty("file.separator");
-//
-//        String currDir = new File("").getAbsolutePath();
-//        String relativePath = separator + "Games" + separator + "Saved" + separator;
-//        String savedPath = new File(currDir).getAbsolutePath() + relativePath;
-//
-//        System.out.println(savedPath);
-//    }
-//
-//
-//}
+
+import java.io.*;
+
+import AdventureModel.AdventureGame;
+import AdventureModel.AdventureLoader;
+import AdventureModel.Interactions.Action;
+import AdventureModel.Interactions.Choice;
+import AdventureModel.Interactions.NPCDialogue;
+import AdventureModel.Interactions.SingleDialogue;
+import AdventureModel.Movement.ForcedQueue;
+import AdventureModel.Movement.Passage;
+import AdventureModel.Movement.Room;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BasicAdventureTest {
+    @Test
+    void getCommandsTest() throws IOException {
+        AdventureGame game = new AdventureGame("TinyGame");
+        String commands = game.player.getCurrentRoom().getCommands();
+        assertEquals("WEST,UP,NORTH,IN,SOUTH,DOWN", commands);
+    }
+
+    @Test
+    void getSavedDir() {
+        String separator = System.getProperty("file.separator");
+
+        String currDir = new File("").getAbsolutePath();
+        String relativePath = separator + "Games" + separator + "Saved" + separator;
+        String savedPath = new File(currDir).getAbsolutePath() + relativePath;
+
+        System.out.println(savedPath);
+    }
+
+    @Test
+    void generalTest(){
+        String str = "SDialogue: You enter the cave, lit dimly by glowing ashes.-true";
+        String[] st = str.split(" ", 2);
+
+        for (String s : st){
+            System.out.println("yuh: " + s);
+        }
+
+        System.out.println(st.length);
+    }
+
+}
