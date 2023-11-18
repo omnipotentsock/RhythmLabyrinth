8e8dfcd06ecbf9837351a7c25d153ac8b84a9191
Ayman Ahmed Mohammed <moha2683@dh2020pc01.utm.utoronto.ca>
2023-11-17 21:09:38 -0500

    REMOVED OBJECTS
    Game runs without AdventureObject Class

--- AdventureModel/AdventureGame.java
+++ AdventureModel/AdventureGame.java
@@ -1,10 +1,5 @@
 package AdventureModel;
 
-import AdventureModel.Characters.Player;
-import AdventureModel.Movement.Passage;
-import AdventureModel.Movement.PassageTable;
-import AdventureModel.Movement.Room;
-
 import java.io.*;
 import java.util.*;
 
@@ -116,6 +111,9 @@ public class AdventureGame implements Serializable {
         //try the blocked passages first
         Passage chosen = null;
         for (Passage entry : possibilities) {
+            System.out.println(entry.getIsBlocked());
+            System.out.println(entry.getKeyName());
+
             chosen = entry; //the passage is unlocked
         }
 
