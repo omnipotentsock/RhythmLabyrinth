89b54cdd17317eed34a2364ff9b954bbcdc5134e
Ayman Ahmed Mohammed <moha2683@dh2020pc22.utm.utoronto.ca>
2023-11-22 18:49:11 -0500

    Added constructors

--- AdventureModel/Interactions/Choice.java
+++ AdventureModel/Interactions/Choice.java
@@ -7,7 +7,28 @@ public class Choice extends Interaction{
     String dialogueText; // String holding text that shows above options
     boolean refreshing = true; // Should the interaction be refreshed
 
-    ArrayList<ChoiceOption> options; // List of options
+    ArrayList<ChoiceOption> options = new ArrayList<ChoiceOption>(); // List of options
+
+    public Choice(String choiceString){
+
+        // Choice: Do you want to attack him?-true-Yes>M0001/No>NULL
+        // At this point, choiceString should be "Do you want to attack him?-true-Yes>M0001/No>NULL"
+
+        String[] choice = choiceString.split("-"); // ["Do you want to attack him?", "true", "Yes>M0001/No>NULL"]
+
+        String dialogueText = choice[0];
+        boolean refresh = Boolean.parseBoolean(choice[1]);
+
+        this.setDialogueText(dialogueText);
+        this.setRefreshing(refresh);
+
+        String[] options = choice[2].split("/");
+
+        // Over here, options should be ["Yes>M0001","No>NULL"]
+        for (String option : options){
+            this.options.add(new ChoiceOption(option));
+        }
+    }
     @Override
     public void execute() {
 
